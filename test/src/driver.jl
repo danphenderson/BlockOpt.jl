2fd85e12e60c3610a3aaf2f02caed55aa98cd669
danphenderson <dphender@mtu.edu>
2021-11-09 23:44:22 -0700

    Passing tests

---
+++ test/src/driver.jl
@@ -0,0 +1,118 @@
+@testset "S Updates" begin
+    S = randn(n, 2w+1)
+    Y = randn(n, 2w+1)
+    p = randn(n)
+
+    # TODO: CRUCIAL ISSUE Y orth to S_new (update D)  
+    # LOOK AT NORMALIZATION
+    S_new = A(S, Y, p); @test S_new'*S_new ≈ I
+    
+    S_new = B(S, Y, p); @test S_new'*S_new ≈ I
+    
+    S_new = C(S, Y, p); @test S_new'*S_new ≈ I
+    
+    S_new = D(S, Y, p); @test S_new'*S_new ≈ I
+    
+    S_new = E(S, Y, p); @test S_new'*S_new ≈ I
+    
+    S_new = F(S, Y, p); @test S_new'*S_new ≈ I
+end
+
+
+@testset "QN Updates" begin
+    U = orth(randn(n, 2w+1))
+    V = ∇²f*U
+    x = randn(n)
+    y = ∇²f*x
+    H = zeros(n, n) + I
+
+    @test U'V ≈ V'U 
+    
+    @test H' ≈ H
+
+    Hnew = SR1(H, U, V, δ); @test Hnew*V ≈ U 
+
+    Hnew = PSB(H, U, V, δ); @test Hnew*V ≈ U
+    
+    Hnew = SR1(H, x, y, δ); @test Hnew*y ≈ x 
+    
+    Hnew = PSB(H, x, y, δ); @test Hnew*y ≈ x 
+end
+
+
+
+
+@testset "testing constructor" begin
+
+    @test S_update(test_driver) ≡ S_UPDATE
+
+    @test QN_update(test_driver) ≡ QN_UPDATE
+    
+    @test pflag(test_driver) ≡ PFLAG
+
+    @test Δ_max(test_driver) ≡ Δ_max(test_options)
+
+    @test δ_tol(test_driver) ≡ δ_tol(test_options)
+
+    @test ϵ_tol(test_driver) ≡ ϵ_tol(test_options)
+
+    @test samples(test_driver) ≡ samples(test_options)
+
+    @test max_iterations(test_driver) ≡ max_iterations(test_options)
+
+    @test weave_level(test_driver) ≡ weave_level(test_options)
+
+    @test log_level(test_driver) ≡ log_level(test_options)
+    
+    restricted_type_test(test_driver);
+end
+
+
+@testset "testing behavior" begin
+
+    # Mutating test_options from test_driver - test update in test_options
+    Δ_max!(test_driver, 0.0)
+    @test Δ_max(test_options) ≡ 0.0
+    
+    δ_tol!(test_driver, 0.0)
+    @test δ_tol(test_options) ≡ 0.0
+    
+    ϵ_tol!(test_driver, 0.0)
+    @test ϵ_tol(test_options) ≡ 0.0
+    
+    samples!(test_driver, 0)
+    @test samples(test_options) ≡ 0
+    
+    max_iterations!(test_driver, 0)
+    @test max_iterations(test_options) ≡ 0
+  
+    weave_level!(test_driver, NONE)
+    @test weave_level(test_options) ≡ NONE
+    
+    log_level!(test_driver, ERROR)
+    @test log_level(test_options) ≡ ERROR
+
+
+    # Mutating test_options from test_options - test update in test_driver
+    Δ_max!(test_options, Δₘ)
+    @test Δ_max(test_driver) ≡ Δₘ
+    
+    δ_tol!(test_options, δ)
+    @test δ_tol(test_driver) ≡ δ
+    
+    ϵ_tol!(test_options, ϵ)
+    @test ϵ_tol(test_driver) ≡ ϵ
+    
+    samples!(test_options, 2w)
+    @test samples(test_driver) ≡ 2w
+    
+    max_iterations!(test_options, N)
+    @test max_iterations(test_driver) ≡ N
+  
+    weave_level!(test_options, WEAVE_LEVEL)
+    @test weave_level(test_driver) ≡ WEAVE_LEVEL
+    
+    log_level!(test_options, LOG_LEVEL)
+    @test log_level(test_driver) ≡ LOG_LEVEL;
+end
+
---
+++ test/src/lib/trace.jl
@@ -0,0 +1,114 @@
+@testset "Counter" begin
+    counter = EvaluationCounter()
+
+    @test evaluations(counter) ≡ 0
+    
+    @test increment!(counter, -1) ≡ 0
+
+    @test increment!(counter)    ≡ 
+          evaluations(counter) ≡ 1
+
+    restricted_type_test(counter)
+end
+
+
+@testset "Timer" begin
+    timer = EvaluationTimer()
+        
+    @test Δt(timer) ≡ 0.0
+
+    on!(timer); sleep(0.5); off!(timer)
+
+    @test Δt(timer) > 0.5
+
+    restricted_type_test(timer);
+end
+
+
+@testset "Profile" begin
+    profile = BlockOptProfile()
+
+    @test Δt(trs_timer(profile)) ≡
+          Δt(ghs_timer(profile)) ≡ 0.0
+   
+    @test evaluations(trs_counter(profile)) ≡
+          evaluations(ghs_counter(profile)) ≡ 0
+ 
+    restricted_type_test(profile)
+end
+
+
+@testset "Weaver" begin
+    weaver = Weaver()
+
+    @test weave_level(weaver) ≡ WEAVE_LEVEL
+
+    weave!(weaver, :f_vals, 0.0)
+
+    @test pop!(f_vals(weaver)) ≡ 0.0
+
+    weave!(weaver, :∇f_norms, 0.0)
+
+    @test pop!(∇f_norms(weaver)) ≡ 0.0
+
+    weave!(weaver, :p_norms, 0.0)
+
+    @test pop!(p_norms(weaver)) ≡ 0.0
+
+    weave!(weaver, :Δ_vals, 0.0)
+
+    @test pop!(Δ_vals(weaver)) ≡ 0.0
+
+    weave!(weaver, :ρ_vals, 0.0)
+
+    @test pop!(ρ_vals(weaver)) ≡ 0.0
+end
+
+
+@testset "Trace" begin
+
+    test_trace = BlockOptTrace(test_model, test_driver)
+
+    @testset "constructor test" begin
+
+        @test model(test_trace) ≡ test_model
+
+        @test driver(test_trace) ≡ test_driver
+
+        @test isa(profile(test_trace), BlockOptProfile)
+
+        @test isa(trs_timer(test_trace), EvaluationTimer)
+
+        @test isa(ghs_timer(test_trace), EvaluationTimer)
+
+        @test isa(trs_counter(test_trace), EvaluationCounter)
+
+        @test isa(ghs_counter(test_trace), EvaluationCounter)
+
+        @test isa(weaver(test_trace), Weaver)
+
+        @test map(isempty, (f_vals(test_trace),
+                            ∇f_norms(test_trace),
+                            Δ_vals(test_trace), 
+                            p_norms(test_trace),
+                            ρ_vals(test_trace))) ≡ (true, true, true, true, true)
+       
+
+        @test weave_level(test_trace) ≡ weave_level(test_driver)
+
+        @test log_level(test_trace) ≡ log_level(test_driver)
+
+        @test isfile(io(test_trace))
+    
+        restricted_type_test(test_trace)
+    end
+
+
+
+    @testset "generated logging function test" begin
+        
+        # TODO: Visual confrimation suggests working as expected
+        
+    end
+
+end
